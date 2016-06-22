8e966df790db7983b23b126eba83c9ee898494e0
corasan <henrypl360@gmail.com>
2016-06-22 08:47:16 -0400

    done

---
+++ index.js
@@ -0,0 +1,9 @@
+var shuffle = require('shuffle-array');
+
+module.exports = function(string) {
+    var arr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".split('');
+    var new_arr = string.split('').map(function(letter) {
+        return letter.replace(letter, shuffle(arr).slice(0,5).join().replace(/,/g, ''));
+    });
+    // console.log(new_arr.join().replace(/,/g, ''));
+}
